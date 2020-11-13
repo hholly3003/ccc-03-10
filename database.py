723aff561e205ededa16ed49d59d0a7a085360ce
Holly <h.holly3003@gmail.com>
2020-11-14 02:58:22 +1100

    split out the database into its own file

---
+++ database.py
@@ -0,0 +1,17 @@
+#this file is specifically about connecting to our databse
+import psycopg2
+import os
+
+#connection setup to the database -- currently setup to the ec2 instance
+connection = psycopg2.connect(
+    database="library_api",
+    user="postgres",
+    password=os.getenv("DB_PASSWORD"),
+    host="54.66.144.62",
+    port="5432"
+)
+
+cursor = connection.cursor()
+
+cursor.execute("create table if not exists books (id serial PRIMARY KEY, title varchar);")
+connection.commit()
